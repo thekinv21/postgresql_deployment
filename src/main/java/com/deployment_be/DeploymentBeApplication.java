ea197796349dba7ad62532272be4e9ef018b56b2
Vadim Kiniabaev <thekinv21@gmail.com>
2024-07-15 15:35:58 +0300

    FEAT:[MASTER-XXX][VADİM]: deployment için proje oluşturuldu

---
+++ src/main/java/com/deployment_be/DeploymentBeApplication.java
@@ -0,0 +1,13 @@
+package com.deployment_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deployment_be/DeploymentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployment_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
